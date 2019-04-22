eac593dc7cbee3c21671dfa0b674f7b09e986c81
tylee <tylee@gwu.edu>
2019-04-22 14:33:54 -0400

    numerical examples for QS aerodynamcis

--- matlab/ani_monarch.m
+++ matlab/ani_monarch.m
@@ -17,35 +17,34 @@ N=301;
 T=5/WK.f;
 t=linspace(0,T,N);
 
+% WK.phi_m=50*pi/180;
+% WK.phi_K=0.4;
+% WK.phi_0=10*pi/180;
+% 
+% WK.theta_m=45*pi/180;
+% WK.theta_C=2;
+% WK.theta_0=0;
+% WK.theta_a=0.3;
+% 
+% WK.psi_m=1*pi/180;
+% WK.psi_N=2;
+% WK.psi_a=0;
+% WK.psi_0=0;
+
+WK.f=50;
+WK.beta=5*pi/180;
+
 WK.phi_m=50*pi/180;
 WK.phi_K=0.4;
 WK.phi_0=10*pi/180;
 
 WK.theta_m=45*pi/180;
-WK.theta_C=2;
+WK.theta_C=5;
 WK.theta_0=0;
 WK.theta_a=0.3;
 
-WK.psi_m=1*pi/180;
+WK.psi_m=10*pi/180;
 WK.psi_N=2;
-WK.psi_a=0;
-WK.psi_0=0;
-
-WK.f=50;
-WK.beta=0*pi/180;
-
-WK.phi_m=60*pi/180;
-WK.phi_K=0.4;
-WK.phi_K=0.9;
-WK.phi_0=10*pi/180;
-
-WK.theta_m=50*pi/180;
-WK.theta_C=4;
-WK.theta_0=0.3;
-WK.theta_a=0;
-
-WK.psi_m=0*pi/180;
-WK.psi_N=0;
 WK.psi_a=0;
 WK.psi_0=0;
 
@@ -69,7 +68,7 @@ h_fig=figure('color','w');
 
 %% animation
 
-for k=floor(linspace(1,N,501))
+for k=floor(linspace(1,N,101))
     [Euler Euler_dot Euler_ddot]=wing_kinematics(t(k),WK);
     [Q_R Q_L W_R W_L W_R_dot W_L_dot]=wing_attitude(WK.beta, Euler, Euler, Euler_dot, Euler_dot, Euler_ddot, Euler_ddot);
     [L_R L_L D_R D_L M_R M_L F_rot_R F_rot_L M_rot_R M_rot_L]=wing_QS_aerodynamics(MONARCH, W_R, W_L, W_R_dot, W_L_dot);
@@ -93,7 +92,7 @@ e2=[0 1 0]';
 lwidth=1;
 alength=12;
 awidth=alength*tand(10);
-scale_Force=0.5e6;
+scale_Force=0.1e5;
 
 x_cp = x + R*Q_R*50*e2;
 acolor=[1 0 0]';
--- matlab/morp_MONARCH.m
+++ matlab/morp_MONARCH.m
@@ -3,16 +3,16 @@ close all;
 
 MONARCH.rho = 1.225; % air density (kg/m^3)
 MONARCH.l = 52.2E-3; % span of the right wing
-MONARCH.S = 2.46E-3/2; % area of the right wing
+MONARCH.S = 2.46E-3; % area of the right wing
 MONARCH.c_bar = MONARCH.S / MONARCH.l; % mean chord
 
-MONARCH.tilde_r_2 = 0.456*MONARCH.l; % non-dimensional radius of the second moment of wing area
-MONARCH.tilde_r_3 = 0.2*MONARCH.l; % non-dimensional radius of the thrid moment of wing area (arbitrary value)
+MONARCH.tilde_r_2 = 0.1; % non-dimensional radius of the second moment of wing area
+MONARCH.tilde_r_3 = 0.1; % non-dimensional radius of the thrid moment of wing area (arbitrary value)
 MONARCH.r_cp = MONARCH.l * MONARCH.tilde_r_3 / MONARCH.tilde_r_2;
 
 MONARCH.tilde_v = 0.1; % non-dimensional virtual mass (arbitrary value)
-MONARCH.tilde_r_v_1 =0.01; % non-dimensional radius of the first moment of wing volume (arbitrary value)
-MONARCH.tilde_r_v_2 =0.01; % non-dimensional radius of the second moment of wing volume (arbitrary value)
+MONARCH.tilde_r_v_1 =0.1; % non-dimensional radius of the first moment of wing volume (arbitrary value)
+MONARCH.tilde_r_v_2 =0.1; % non-dimensional radius of the second moment of wing volume (arbitrary value)
 MONARCH.r_rot = MONARCH.l * MONARCH.tilde_r_v_2^2 / MONARCH.tilde_r_v_1; % non-dimensional radius of the second moment of wing volume (arbitrary value)
 
 save('morp_MONARCH');
