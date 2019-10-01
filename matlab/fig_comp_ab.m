9da9e90d032f057e0dc5b84e4ee51db962019f31
tylee <tylee@gwu.edu>
2019-10-01 09:57:33 -0400

    explanation to compute K

--- matlab/fig_comp_ab.m
+++ matlab/fig_comp_ab.m
@@ -1,7 +1,9 @@
-clear all;
+function fig_comp_ab
 close all;
 
 load sim_QS_x_wo_ab;
+[pow, E, E_dot, eff] = compute_power(MONARCH.m, t, x, x_dot, tau, Q_R, Q_L, Q_A, W_R, W_L, W_A);
+
 
 h_x3=figure;
 plot(x(1,:),x(3,:),'g');
@@ -33,10 +35,40 @@ subplot(2,1,2);
 plot(t*WK.f,theta_A*180/pi,'g');
 hold on;
 
+h_tau=figure;
+subplot(3,1,1);
+plot(t*WK.f, tau(4:6,:),'g');
+hold on;
+subplot(3,1,2);
+plot(t*WK.f, tau(7:9,:),'g');
+hold on;
+subplot(3,1,3);
+plot(t*WK.f, tau(11,:),'g');
+hold on;
+
+h_pow = figure;
+for ii=1:3
+    subplot(3,1,ii);
+    plot(t*WK.f,pow(ii,:),'g');
+    hold on;
+end
+
+h_E = figure;
+subplot(3,1,1);
+plot(t*WK.f,E,'g');
+hold on;
+subplot(3,1,2);
+plot(t*WK.f,E_dot,'g');
+hold on;
+subplot(3,1,3);
+plot(t*WK.f,eff,'g');
+hold on;
 
 
 %%
 load sim_QS_x_with_ab;
+[pow, E, E_dot, eff] = compute_power(MONARCH.m, t, x, x_dot, tau, Q_R, Q_L, Q_A, W_R, W_L, W_A);
+
 
 figure(h_x3);
 plot(x(1,:),x(3,:),'b');
@@ -86,9 +118,38 @@ plot(t*WK.f,theta_A*180/pi,'b');
 ylabel('$\theta_A$','interpreter','latex');
 xlabel('$t/T$','interpreter','latex');
 
+figure(h_tau);
+subplot(3,1,1);
+plot(t*WK.f, tau(4:6,:),'b');
+ylabel('$\tau_R$','interpreter','latex');
+subplot(3,1,2);
+plot(t*WK.f, tau(7:9,:),'b');
+ylabel('$\tau_L$','interpreter','latex');
+subplot(3,1,3);
+plot(t*WK.f, tau(11,:),'b');
+ylabel('$\tau_A$','interpreter','latex');
+
+figure(h_pow);
+for ii=1:3
+    subplot(3,1,ii);
+    plot(t*WK.f,pow(ii,:),'b');
+    hold on;
+end
+
+figure(h_E);
+subplot(3,1,1);
+plot(t*WK.f,E,'b');
+subplot(3,1,2);
+plot(t*WK.f,E_dot,'b');
+subplot(3,1,3);
+plot(t*WK.f,eff,'b');
+
+
+
 
 %%
 load sim_QS_x_with_opposite_ab;
+[pow, E, E_dot, eff] = compute_power(MONARCH.m, t, x, x_dot, tau, Q_R, Q_L, Q_A, W_R, W_L, W_A);
 
 figure(h_x3);
 plot(x(1,:),x(3,:),'m');
@@ -122,11 +183,47 @@ plot(t*WK.f,theta_A*180/pi,'m');
 xlim([0 5]);
 patch_downstroke(h_theta,t*WK.f,Euler_R_dot);
 
+figure(h_tau);
+subplot(3,1,1);
+plot(t*WK.f, tau(4:6,:),'m');
+xlim([0 5]);
+patch_downstroke(h_tau,t*WK.f,Euler_R_dot);
+subplot(3,1,2);
+plot(t*WK.f, tau(7:9,:),'m');
+xlim([0 5]);
+patch_downstroke(h_tau,t*WK.f,Euler_R_dot);
+subplot(3,1,3);
+plot(t*WK.f, tau(11,:),'m');
+xlim([0 5]);
+patch_downstroke(h_tau,t*WK.f,Euler_R_dot);
+
+figure(h_pow);
+for ii=1:3
+    subplot(3,1,ii);
+    plot(t*WK.f,pow(ii,:),'m');
+    xlim([0 5]);
+    patch_downstroke(h_pow,t*WK.f,Euler_R_dot);
+    hold on;
+end
+
+figure(h_E);
+subplot(3,1,1);
+plot(t*WK.f,E,'m');
+xlim([0 5]);
+patch_downstroke(h_E,t*WK.f,Euler_R_dot);
+subplot(3,1,2);
+plot(t*WK.f,E_dot,'m');
+xlim([0 5]);
+patch_downstroke(h_E,t*WK.f,Euler_R_dot);
+subplot(3,1,3);
+plot(t*WK.f,eff,'m');
+xlim([0 5]);
+patch_downstroke(h_E,t*WK.f,Euler_R_dot);
 
 
 %%
 
-bool_print=true;
+bool_print=false;
 figname='comp_ab';
 if bool_print
     figure(h_x3);print([figname '_x3'],'-depsc');
@@ -134,5 +231,32 @@ if bool_print
     figure(h_v);print([figname '_v'],'-depsc');
     figure(h_v3);print([figname '_v3'],'-depsc');
     figure(h_theta);print([figname '_theta'],'-depsc');
+    !mv comp_ab*.eps ../doc/Figs/
 end
-!mv comp_ab*.eps ../doc/Figs/
+
+end
+
+
+function [pow, E, E_dot, eff] = compute_power(m, t, x, x_dot, tau, Q_R, Q_L, Q_A, W_R, W_L, W_A)
+
+N=length(t);
+pow=zeros(3,N);
+for k=1:N
+    pow(1,k) = tau(4:6,k)'*Q_R(:,:,k)*W_R(:,k); 
+    pow(2,k) = tau(7:9,k)'*Q_L(:,:,k)*W_L(:,k); 
+    pow(3,k) = tau(10:12,k)'*Q_A(:,:,k)*W_A(:,k); 
+    E(k) = 0.5*m*x_dot(:,k)'*x_dot(:,k) - m*9.81*x(3);
+end
+
+E_dot = diff(E')./diff(t);
+E_dot = [E_dot; E_dot(end)];
+
+for k=1:N
+    eff(k) = E_dot(k)/sum(pow(:,k));
+end
+
+
+
+end
+
+
