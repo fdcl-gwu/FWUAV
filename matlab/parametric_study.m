3d59397d8c28413e1c8d43494f97e21811d1f10e
Tejaswi K C <kctejaswi999@gmail.com>
2020-01-21 14:50:09 -0500

    Introduced optimization for control gains

--- matlab/parametric_study.m
+++ matlab/parametric_study.m
@@ -13,81 +13,91 @@ N=1001;
 T=3/WK.f;
 t=linspace(0,T,N);
 
-N_params = 15;
+N_params = 10;
 eps = logspace(log10(0.0001), log10(0.1), N_params);
-f_a_m = zeros(3, 3, N_params);
+f_aero = zeros(3, 3, N, N_params);
+f_a_m = zeros(3, 3, N_params, 2);
 
 parfor i=1:N_params
-    f_a_m(:, :, i) = param_study(INSECT, WK, eps, i, t, X0, N);
-    disp(i);
+    f_aero(:, :, :, i) = param_study(INSECT, WK, eps, i, t, X0, N);
+    f_aero1 = f_aero(:, :, :, i); f_aero2 = f_aero(:, :, :, i);
+    f_aero1(f_aero1 < 0) = 0;
+    f_aero2(f_aero2 > 0) = 0;
+    
+    f_a_m_temp = zeros(size(f_a_m(:, :, i, :)));
+    f_a_m_temp(:, :, 1, 1) = mean(f_aero1, 3);
+    f_a_m_temp(:, :, 1, 2) = mean(f_aero2, 3);
+    f_a_m(:, :, i, :) = f_a_m_temp;
 end
+f_a_m = abs(f_a_m);
 
+%%
 h_f_a = figure;
 h_f_a.PaperUnits = 'inches';
 h_f_a.PaperPosition = [0 0 12 9];
 %
 subplot(3,3,1);
-plot(eps, squeeze(f_a_m(1,1,:)));
+plot(eps, squeeze(f_a_m(1,1,:,1)));
 hold on;
-scatter(eps, squeeze(f_a_m(1,1,:)), 10, 'k', 'filled');
-xlabel('$\epsilon\ \vert\ \Delta\phi_{m, R} = \epsilon, \Delta\phi_{m, L} = \epsilon$','interpreter','latex');
-ylabel('mean $\|f_a(1)\|$ along $x$','interpreter','latex');
+plot(eps, squeeze(f_a_m(1,1,:,2)), 'r');
+legend('Positive component', 'Negative component');
+% scatter(eps, squeeze(f_a_m(1,1,:,1)), 10, 'k', 'filled');
+% xlabel('$\epsilon\ \vert\ \Delta\phi_{m, R} = \epsilon, \Delta\phi_{m, L} = \epsilon$','interpreter','latex');
+ylabel('mean $f_a(1)$','interpreter','latex');
 
 subplot(3,3,4);
-plot(eps, squeeze(f_a_m(1,2,:)));
+plot(eps, squeeze(f_a_m(1,2,:,1)));
 hold on;
-scatter(eps, squeeze(f_a_m(1,2,:)), 10, 'k', 'filled');
-xlabel('$\epsilon\ \vert\ \Delta\phi_{m, R} = \epsilon, \Delta\phi_{m, L} = \epsilon$','interpreter','latex');
-ylabel('mean $\|f_a(2)\|$ along $y$','interpreter','latex');
+plot(eps, squeeze(f_a_m(1,2,:,2)), 'r');
+% xlabel('$\epsilon\ \vert\ \Delta\phi_{m, R} = \epsilon, \Delta\phi_{m, L} = \epsilon$','interpreter','latex');
+ylabel('mean $f_a(2)$','interpreter','latex');
 
 subplot(3,3,7);
-plot(eps, squeeze(f_a_m(1,3,:)));
-hold on;
-scatter(eps, squeeze(f_a_m(1,3,:)), 10, 'k', 'filled');
+% plot(eps, squeeze(f_a_m(1,3,:,1)));
+% hold on;
+plot(eps, squeeze(f_a_m(1,3,:,2)), 'r');
 xlabel('$\epsilon\ \vert\ \Delta\phi_{m, R} = \epsilon, \Delta\phi_{m, L} = \epsilon$','interpreter','latex');
-ylabel('mean $\|f_a(3)\|$ along $z$','interpreter','latex');
+ylabel('mean $f_a(3)$','interpreter','latex');
 %
 subplot(3,3,2);
-plot(eps, squeeze(f_a_m(2,1,:)));
+plot(eps, squeeze(f_a_m(2,1,:,1)));
 hold on;
-scatter(eps, squeeze(f_a_m(2,1,:)), 10, 'k', 'filled');
-xlabel('$\epsilon\ \vert\ \Delta\theta_{m, R} = \epsilon, \Delta\theta_{m, L} = \epsilon$','interpreter','latex');
-ylabel('mean $\|f_a(1)\|$ along $x$','interpreter','latex');
+plot(eps, squeeze(f_a_m(2,1,:,2)), 'r');
+legend('Positive component', 'Negative component');
+% xlabel('$\epsilon\ \vert\ \Delta\theta_{m, R} = \epsilon, \Delta\theta_{m, L} = \epsilon$','interpreter','latex');
+% ylabel('mean $f_a(1)$','interpreter','latex');
 
 subplot(3,3,5);
-plot(eps, squeeze(f_a_m(2,2,:)));
+plot(eps, squeeze(f_a_m(2,2,:,1)));
 hold on;
-scatter(eps, squeeze(f_a_m(2,2,:)), 10, 'k', 'filled');
-xlabel('$\epsilon\ \vert\ \Delta\theta_{m, R} = \epsilon, \Delta\theta_{m, L} = \epsilon$','interpreter','latex');
-ylabel('mean $\|f_a(2)\|$ along $y$','interpreter','latex');
+plot(eps, squeeze(f_a_m(2,2,:,2)), 'r');
+% xlabel('$\epsilon\ \vert\ \Delta\theta_{m, R} = \epsilon, \Delta\theta_{m, L} = \epsilon$','interpreter','latex');
+% ylabel('mean $f_a(2)$','interpreter','latex');
 
 subplot(3,3,8);
-plot(eps, squeeze(f_a_m(2,3,:)));
-hold on;
-scatter(eps, squeeze(f_a_m(2,3,:)), 10, 'k', 'filled');
+plot(eps, squeeze(f_a_m(2,3,:,2)), 'r');
 xlabel('$\epsilon\ \vert\ \Delta\theta_{m, R} = \epsilon, \Delta\theta_{m, L} = \epsilon$','interpreter','latex');
-ylabel('mean $\|f_a(3)\|$ along $z$','interpreter','latex');
+% ylabel('mean $f_a(3)$','interpreter','latex');
 %
 subplot(3,3,3);
-plot(eps, squeeze(f_a_m(3,1,:)));
+plot(eps, squeeze(f_a_m(3,1,:,1)));
 hold on;
-scatter(eps, squeeze(f_a_m(3,1,:)), 10, 'k', 'filled');
-xlabel('$\epsilon\ \vert\ \Delta\psi_{m, R} = \epsilon, \Delta\psi_{m, L} = -\epsilon$','interpreter','latex');
-ylabel('mean $\|f_a(1)\|$ along $x$','interpreter','latex');
+plot(eps, squeeze(f_a_m(3,1,:,2)), 'r');
+legend('Positive component', 'Negative component');
+% xlabel('$\epsilon\ \vert\ \Delta\psi_{m, R} = \epsilon, \Delta\psi_{m, L} = -\epsilon$','interpreter','latex');
+% ylabel('mean $f_a(1)$','interpreter','latex');
 
 subplot(3,3,6);
-plot(eps, squeeze(f_a_m(3,2,:)));
+plot(eps, squeeze(f_a_m(3,2,:,1)));
 hold on;
-scatter(eps, squeeze(f_a_m(3,2,:)), 10, 'k', 'filled');
-xlabel('$\epsilon\ \vert\ \Delta\psi_{m, R} = \epsilon, \Delta\psi_{m, L} = -\epsilon$','interpreter','latex');
-ylabel('mean $\|f_a(2)\|$ along $y$','interpreter','latex');
+plot(eps, squeeze(f_a_m(3,2,:,2)), 'r');
+% xlabel('$\epsilon\ \vert\ \Delta\psi_{m, R} = \epsilon, \Delta\psi_{m, L} = -\epsilon$','interpreter','latex');
+% ylabel('mean $f_a(2)$','interpreter','latex');
 
 subplot(3,3,9);
-plot(eps, squeeze(f_a_m(3,3,:)));
-hold on;
-scatter(eps, squeeze(f_a_m(3,3,:)), 10, 'k', 'filled');
+plot(eps, squeeze(f_a_m(3,3,:,2)), 'r');
 xlabel('$\epsilon\ \vert\ \Delta\psi_{m, R} = \epsilon, \Delta\psi_{m, L} = -\epsilon$','interpreter','latex');
-ylabel('mean $\|f_a(3)\|$ along $z$','interpreter','latex');
+% ylabel('mean $f_a(3)$','interpreter','latex');
 %
 % print(h_f_a, 'hover_param_study', '-depsc', '-r0');
 
@@ -101,26 +111,26 @@ save(filename, allvars(tosave).name)
 evalin('base',['load ' filename]);
 end
 
-function f_a_m = param_study(INSECT, WK, eps, i, t, X0, N)
-    f_a_m = zeros(3, 3);
+function f_aero = param_study(INSECT, WK, eps, i, t, X0, N)
+    f_aero = zeros(3, 3, N);
     %
     WK_R = WK;  WK_L = WK;
     WK_R.phi_m = WK_R.phi_m + eps(i);
     WK_L.phi_m = WK_L.phi_m + eps(i);
-    f_a_m(1, :) = aerodynamic_force(INSECT, WK_R, WK_L, t, X0, N);
+    f_aero(1, :, :) = aerodynamic_force(INSECT, WK_R, WK_L, t, X0, N);
     %
     WK_R = WK;  WK_L = WK;
     WK_R.theta_m = WK_R.theta_m + eps(i);
     WK_L.theta_m = WK_L.theta_m + eps(i);
-    f_a_m(2, :) = aerodynamic_force(INSECT, WK_R, WK_L, t, X0, N);
+    f_aero(2, :, :) = aerodynamic_force(INSECT, WK_R, WK_L, t, X0, N);
     %
     WK_R = WK;  WK_L = WK;
     WK_R.psi_m = WK_R.psi_m + eps(i);
     WK_L.psi_m = WK_L.psi_m - eps(i);
-    f_a_m(3, :) = aerodynamic_force(INSECT, WK_R, WK_L, t, X0, N);
+    f_aero(3, :, :) = aerodynamic_force(INSECT, WK_R, WK_L, t, X0, N);
 end
 
-function f_a_m = aerodynamic_force(INSECT, WK_R, WK_L, t, X0, N)
+function f_aero = aerodynamic_force(INSECT, WK_R, WK_L, t, X0, N)
     [t X]=ode45(@(t,X) eom(INSECT, WK_R, WK_L, t, X), t, X0, odeset('AbsTol',1e-6,'RelTol',1e-6));
 
 %     x=X(:,1:3)';
@@ -129,8 +139,7 @@ function f_a_m = aerodynamic_force(INSECT, WK_R, WK_L, t, X0, N)
     for k=1:N    
         [~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, f_a(:,k)]= eom(INSECT, WK_R, WK_L, t(k), X(k,:)');
     end
-    f_a_m = mean(abs(f_a(1:3, :)), 2);
-%     F_R_m(:, i) = mean(abs(F_R(1:3, :)), 2);
+    f_aero = f_a(1:3, :);
 end
 
 function [X_dot R Q_R Q_L Q_A theta_B theta_A W W_dot W_R W_R_dot W_L W_L_dot W_A W_A_dot F_R F_L M_R M_L f_a f_g f_tau tau]= eom(INSECT, WK_R, WK_L, t, X)
@@ -142,9 +151,6 @@ x_dot=X(4:6);
 [Euler_R, Euler_R_dot, Euler_R_ddot] = wing_kinematics(t,WK_R);
 [Euler_L, Euler_L_dot, Euler_L_ddot] = wing_kinematics(t,WK_L);
 [Q_R Q_L W_R W_L W_R_dot W_L_dot] = wing_attitude(WK_R.beta, Euler_R, Euler_L, Euler_R_dot, Euler_L_dot, Euler_R_ddot, Euler_L_ddot);
-
-% [R W W_dot theta_B] = body_attitude(t,WK_R.f); %time-varying thorax
-% [Q_A W_A W_A_dot theta_A] = abdomen_attitude(17.32*pi/180); % fixed abdomen
 
 [R W W_dot theta_B] = body_attitude(t, WK_R.f, WK_R); % body
 [Q_A W_A W_A_dot theta_A] = abdomen_attitude(t, WK_R.f, WK_R); % abdomen
--- matlab/sim_QS_x_hover_control.m
+++ matlab/sim_QS_x_hover_control.m
@@ -3,16 +3,16 @@ function sim_QS_x_hover_control
 % given thorax attiude, wing kinematics, abdomen attitude.
 
 evalin('base','clear all');
-% close all;
+close all;
 addpath('./modules', './sim_data');
 des=load('sim_QS_x_hover.mat',...
     'INSECT', 't', 'N', 'x', 'x_dot', 'R', 'Q_R', 'Q_L', 'W_R', 'W_L', 'f_tau',...
-    'x0', 'x_dot0', 'Q_A', 'WK');
+    'x0', 'x_dot0', 'Q_A', 'WK', 'x_ddot');
 
 filename='sim_QS_x_hover_control';
 INSECT = des.INSECT;
 WK = des.WK;
-des.x_fit = cell(3, 1); des.x_dot_fit = cell(3, 1);
+des.x_fit = cell(3, 1); des.x_dot_fit = cell(3, 1); des.x_ddot_fit = cell(3, 1);
 % fttype = "a0 + ";
 % for i=1:20
 %     fttype = fttype + 'a' + string(i) + '*cos(' + string(i) + '*x*w) + b'...
@@ -23,6 +23,7 @@ des.x_fit = cell(3, 1); des.x_dot_fit = cell(3, 1);
 for i=1:3
     des.x_fit{i} = fit(des.t, des.x(i, :)', 'fourier8');
     des.x_dot_fit{i} = fit(des.t, des.x_dot(i, :)', 'fourier8');
+    des.x_ddot_fit{i} = fit(des.t, des.x_ddot(i, :)', 'fourier8');
 end
 
 % Values obtained from parametric study
@@ -42,21 +43,39 @@ des.df_a_3_by_dphi_m = 0.47e-3 / 0.1; % dphi_m_R > 0, dphi_m_L > 0
 gains.Kp_pos = -2;
 gains.Kd_pos = 2;
 % New
-gains.Kp_pos = 2;
-gains.Kd_pos = 2;
-gains.Ki_pos = 0;
+gains.Kp_pos = 10;
+gains.Kd_pos = 5;
+gains.Ki_pos = 0.1;
 
-%% Simulation
 eps1 = 1e-2; eps2 = 1e-1;
 x0 = des.x0 + rand(3,1)*eps1;
 x_dot0 = des.x_dot0 + rand(3,1)*eps2;
 X0 = [x0; x_dot0;];
-
 N = 2001;
 N_period = 20;
 N_single = round((N-1)/N_period);
 T = N_period/WK.f;
 t = linspace(0,T,N);
+
+% g0 = [gains.Kp_pos, gains.Kd_pos, gains.Ki_pos];
+% lb = [-1000, -1000, -100];
+% ub = [1000, 1000, 100];
+% tic;
+% % rng default; % For reproducibility
+% options = optimoptions(@surrogateopt, 'InitialPoints', g0,...
+%     'MaxFunctionEvaluations', 50000, 'MaxTime', 10000, ...
+%     'PlotFcn', @surrogateoptplot);
+% [gs, fval, exitflag, output] = surrogateopt(@(gs) obtain_err_gains(gs, WK, INSECT, des, X0, N, t), lb, ub, options);
+% fprintf('Optimization has been completed\n');
+% disp(fval);
+% disp(output);
+% toc;
+gs = [427.1529   15.6076  -13.4983];
+
+%% Simulation
+gains.Kp_pos = gs(1);
+gains.Kd_pos = gs(2);
+gains.Ki_pos = gs(3);
 
 % [t, X]=ode45(@(t,X) eom(INSECT, WK, WK, t, X, des, gains, i), t, X0, odeset('AbsTol',1e-6,'RelTol',1e-6));
 
@@ -68,25 +87,54 @@ for i=1:(N-1)
 %     if mod(i-1, N_single) == 0
 %         x0 = X(i, 1:3)';
 %     end
-    k1 = dt * eom(INSECT, WK, WK, t(i), X(i, :)', des, gains, i, x0);
-    k2 = dt * eom(INSECT, WK, WK, t(i)+dt/2, X(i, :)'+k1/2, des, gains, i, x0);
-    k3 = dt * eom(INSECT, WK, WK, t(i)+dt/2, X(i, :)'+k2/2, des, gains, i, x0);
-    k4 = dt * eom(INSECT, WK, WK, t(i), X(i, :)'+k3, des, gains, i, x0);
-    X(i+1, :) = X(i, :) + 1/6 * (k1 + 2*k2 + 2*k3 + k4)';
+    if i == 1
+        f_a_im1 = zeros(3, 1);
+        x_ddot = zeros(3, 1);
+    else
+        f_a_im1 = f_a(1:3, i-1);
+        x_ddot = X_dot(4:6, i-1);
+    end
+    %
+%     k1 = dt * eom(INSECT, WK, WK, t(i), X(i, :)', des, gains, i, x0, f_a_im1);
+%     k2 = dt * eom(INSECT, WK, WK, t(i)+dt/2, X(i, :)'+k1/2, des, gains, i, x0, f_a_im1);
+%     k3 = dt * eom(INSECT, WK, WK, t(i)+dt/2, X(i, :)'+k2/2, des, gains, i, x0, f_a_im1);
+%     k4 = dt * eom(INSECT, WK, WK, t(i), X(i, :)'+k3, des, gains, i, x0, f_a_im1);
+%     X(i+1, :) = X(i, :) + 1/6 * (k1 + 2*k2 + 2*k3 + k4)';
+    %
+    [X_dot(:,i), R(:,:,i) Q_R(:,:,i) Q_L(:,:,i) Q_A(:,:,i) theta_B(i) theta_A(i) ...
+        W(:,i) W_dot(:,i) W_R(:,i) W_R_dot(:,i) W_L(:,i) W_L_dot(:,i) W_A(:,i) ...
+        W_A_dot(:,i) F_R(:,i) F_L(:,i) M_R(:,i) M_L(:,i) f_a(:,i) f_g(:,i) ...
+        f_tau(:,i) tau(:,i) Euler_R(:,i) Euler_R_dot(:,i) pos_err(:, i)]... 
+        = eom(INSECT, WK, WK, t(i), X(i,:)', des, gains, i, x0, f_a_im1, x_ddot);
+    X(i+1, :) = X(i, :) + dt * X_dot(:, i)';
 end
+i = i+1;
+[X_dot(:,i), R(:,:,i) Q_R(:,:,i) Q_L(:,:,i) Q_A(:,:,i) theta_B(i) theta_A(i) ...
+        W(:,i) W_dot(:,i) W_R(:,i) W_R_dot(:,i) W_L(:,i) W_L_dot(:,i) W_A(:,i) ...
+        W_A_dot(:,i) F_R(:,i) F_L(:,i) M_R(:,i) M_L(:,i) f_a(:,i) f_g(:,i) ...
+        f_tau(:,i) tau(:,i) Euler_R(:,i) Euler_R_dot(:,i) pos_err(:, i)]... 
+        = eom(INSECT, WK, WK, t(i), X(i,:)', des, gains, i, x0, f_a_im1, x_ddot);
 
 x=X(:,1:3)';
 x_dot=X(:,4:6)';
 
-R=zeros(3,3,N);
-for k=1:N
-%     if mod(k-1, N_single) == 0
-%         x0 = X(k, 1:3)';
-%     end
-    [X_dot(:,k), R(:,:,k) Q_R(:,:,k) Q_L(:,:,k) Q_A(:,:,k) theta_B(k) theta_A(k) W(:,k) W_dot(:,k) W_R(:,k) W_R_dot(:,k) W_L(:,k) W_L_dot(:,k) W_A(:,k) W_A_dot(:,k) F_R(:,k) F_L(:,k) M_R(:,k) M_L(:,k) f_a(:,k) f_g(:,k) f_tau(:,k) tau(:,k) Euler_R(:,k) Euler_R_dot(:,k) pos_err(:, k)]= eom(INSECT, WK, WK, t(k), X(k,:)', des, gains, k, x0);
-    F_B(:,k)=Q_R(:,:,k)*F_R(:,k) + Q_L(:,:,k)*F_L(:,k);
+for j=1:3
+    err_pos(:, j) = des.x_fit{j}(t) - x(j, :)';
+    err_vel(:, j) = des.x_dot_fit{j}(t) - x_dot(j, :)';
 end
-x_ddot = X_dot(4:6,:);
+err_pos = vecnorm(err_pos, 2, 2);
+err_vel = vecnorm(err_vel, 2, 2);
+err_pos = trapz(t, err_pos) / max(t);
+err_vel = trapz(t, err_vel) / max(t);
+
+% for k=1:N
+% %     if mod(k-1, N_single) == 0
+% %         x0 = X(k, 1:3)';
+% %     end
+%     [X_dot(:,k), R(:,:,k) Q_R(:,:,k) Q_L(:,:,k) Q_A(:,:,k) theta_B(k) theta_A(k) W(:,k) W_dot(:,k) W_R(:,k) W_R_dot(:,k) W_L(:,k) W_L_dot(:,k) W_A(:,k) W_A_dot(:,k) F_R(:,k) F_L(:,k) M_R(:,k) M_L(:,k) f_a(:,k) f_g(:,k) f_tau(:,k) tau(:,k) Euler_R(:,k) Euler_R_dot(:,k) pos_err(:, k)]= eom(INSECT, WK, WK, t(k), X(k,:)', des, gains, k, x0);
+%     F_B(:,k)=Q_R(:,:,k)*F_R(:,k) + Q_L(:,:,k)*F_L(:,k);
+% end
+% x_ddot = X_dot(4:6,:);
 
 %% Figures
 h_x=figure;
@@ -101,7 +149,7 @@ xlabel('$t/T$','interpreter','latex');
 subplot(3,1,2);
 ylabel('$x$','interpreter','latex');
 % print(h_x, 'hover_control_pos', '-depsc');
-
+% 
 h_x_dot=figure;
 for ii=1:3 
     subplot(3,1,ii);
@@ -115,7 +163,7 @@ xlabel('$t/T$','interpreter','latex');
 subplot(3,1,2);
 ylabel('$\dot x$','interpreter','latex');
 % print(h_x_dot, 'hover_control_vel', '-depsc');
-
+% 
 h_err = figure;
 subplot(3,2,1);
 plot(t*WK.f, pos_err(1,:));
@@ -152,22 +200,78 @@ evalin('base',['load ' filename]);
 
 end
 
-function [X_dot R Q_R Q_L Q_A theta_B theta_A W W_dot W_R W_R_dot W_L W_L_dot W_A W_A_dot F_R F_L M_R M_L f_a f_g f_tau tau Euler_R Euler_R_dot pos_err]= eom(INSECT, WK_R, WK_L, t, X, des, gains, i, x0)
+function err =  obtain_err_gains(gs, WK, INSECT, des, X0, N, t)
+    gains.Kp_pos = gs(1);
+    gains.Kd_pos = gs(2);
+    gains.Ki_pos = gs(3);
+
+    X = zeros(N, 9);
+    X(1, :) = [X0; zeros(3, 1)];
+    dt = t(2) - t(1);
+    for i=1:(N-1)
+        if i == 1
+            f_a_im1 = zeros(3, 1);
+            x_ddot = zeros(3, 1);
+        else
+            f_a_im1 = f_a(1:3, i-1);
+            x_ddot = X_dot(4:6, i-1);
+        end
+        %
+    %     k1 = dt * eom(INSECT, WK, WK, t(i), X(i, :)', des, gains, i, x0, f_a_im1);
+    %     k2 = dt * eom(INSECT, WK, WK, t(i)+dt/2, X(i, :)'+k1/2, des, gains, i, x0, f_a_im1);
+    %     k3 = dt * eom(INSECT, WK, WK, t(i)+dt/2, X(i, :)'+k2/2, des, gains, i, x0, f_a_im1);
+    %     k4 = dt * eom(INSECT, WK, WK, t(i), X(i, :)'+k3, des, gains, i, x0, f_a_im1);
+    %     X(i+1, :) = X(i, :) + 1/6 * (k1 + 2*k2 + 2*k3 + k4)';
+        %
+        [X_dot(:,i), R(:,:,i) Q_R(:,:,i) Q_L(:,:,i) Q_A(:,:,i) theta_B(i) theta_A(i) ...
+            W(:,i) W_dot(:,i) W_R(:,i) W_R_dot(:,i) W_L(:,i) W_L_dot(:,i) W_A(:,i) ...
+            W_A_dot(:,i) F_R(:,i) F_L(:,i) M_R(:,i) M_L(:,i) f_a(:,i) f_g(:,i) ...
+            f_tau(:,i) tau(:,i) Euler_R(:,i) Euler_R_dot(:,i) pos_err(:, i)]... 
+            = eom(INSECT, WK, WK, t(i), X(i,:)', des, gains, i, X0(1:3), f_a_im1, x_ddot);
+        X(i+1, :) = X(i, :) + dt * X_dot(:, i)';
+    end
+
+    x=X(:,1:3)';
+    x_dot=X(:,4:6)';
+
+    err_pos = zeros(N, 3); err_vel = zeros(N, 3);
+    for j=1:3
+        err_pos(:, j) = des.x_fit{j}(t) - x(j, :)';
+        err_vel(:, j) = des.x_dot_fit{j}(t) - x_dot(j, :)';
+    end
+    err_pos = vecnorm(err_pos, 2, 2);
+    err_vel = vecnorm(err_vel, 2, 2);
+    err_pos_dot = diff(err_pos)./diff(t');
+    err_pos_dot = [err_pos_dot; err_pos_dot(end);];
+    err_vel_dot = diff(err_vel)./diff(t');
+    err_vel_dot = [err_vel_dot; err_vel_dot(end);];
+    err_pos_dot = trapz(t', abs(err_pos_dot)) / max(t);
+    err_vel_dot = trapz(t', abs(err_vel_dot)) / max(t);
+    err_pos = trapz(t((N-1)/2:end)', err_pos((N-1)/2:end)) / max(t);
+    err_vel = trapz(t((N-1)/2:end)', err_vel((N-1)/2:end)) / max(t);
+    err = (1000*err_pos + 100*err_vel) + 0.01*(1000*err_pos_dot + 100*err_vel_dot);
+    if isnan(err)
+        err = 1e10;
+    end
+end
+
+function [X_dot R Q_R Q_L Q_A theta_B theta_A W W_dot W_R W_R_dot W_L W_L_dot W_A W_A_dot F_R F_L M_R M_L f_a f_g f_tau tau Euler_R Euler_R_dot pos_err]= eom(INSECT, WK_R, WK_L, t, X, des, gains, i, x0, f_a_im1, x_ddot)
 x=X(1:3);
 x_dot=X(4:6);
 int_d_x=X(7:9);
 
 % Control design
-d_x = zeros(3, 1); d_x_dot = zeros(3, 1);
+d_x = zeros(3, 1); d_x_dot = zeros(3, 1); d_x_ddot = zeros(3, 1);
 for j=1:3
 %     d_x(j) = des.x_fit{j}(t) - (x(j) - x0(j));
     d_x(j) = des.x_fit{j}(t) - x(j);
     d_x_dot(j) = des.x_dot_fit{j}(t) - x_dot(j);
+%     d_x_ddot(j) = des.x_ddot_fit{j}(t) - x_ddot(j);
 end
 pos_err = INSECT.m*(gains.Kp_pos * d_x + gains.Kd_pos * d_x_dot + gains.Ki_pos * int_d_x);
-dphi_m = - pos_err(3) / des.df_a_3_by_dphi_m;
-dtheta_m = (pos_err(1) + dphi_m * des.df_a_1_by_dphi_m) / des.df_a_1_by_dtheta_m;
-dpsi_m = pos_err(2) / des.df_a_2_by_dpsi_m;
+dphi_m = sign(f_a_im1(3)) *  pos_err(3) / des.df_a_3_by_dphi_m;
+dtheta_m = sign(f_a_im1(1)) * (pos_err(1) - sign(f_a_im1(1)) * dphi_m * des.df_a_1_by_dphi_m) / des.df_a_1_by_dtheta_m;
+dpsi_m = sign(f_a_im1(2)) * pos_err(2) / des.df_a_2_by_dpsi_m;
 
 WK_R.phi_m = WK_R.phi_m + dphi_m;
 WK_L.phi_m = WK_L.phi_m + dphi_m;
