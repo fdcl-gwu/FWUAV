6ebdc51fcdda200358e74c72d2f430173336b3ad
Tejaswi K C <kctejaswi999@gmail.com>
2020-06-24 15:55:16 -0400

    (x,R) param study completed

--- matlab/parametric_study_xR.m
+++ matlab/parametric_study_xR.m
@@ -13,12 +13,12 @@ T=1/WK.f;
 t=linspace(0,T,N);
 
 N_params = 50;
-N_study = 5;
+N_study = 7;
 eps = linspace(-0.1, 0.1, N_params);
 f_aero = zeros(N_study, 3, N, N_params);
 M_aero = zeros(N_study, 3, N, N_params);
 f_a_m = zeros(N_study, 3, N_params, 2);
-M_a_m = zeros(N_study, 3, N_params);
+M_a_m = zeros(N_study, 3, N_params, 2);
 
 [t,X]=ode45(@(t,X) eom_QS_xR(INSECT, WK, WK, t, X), t, X0, odeset('AbsTol',1e-6,'RelTol',1e-6));
 for k=1:N
@@ -37,25 +37,57 @@ parfor i=1:N_params
     f_a_m_temp(:, :, 1, 2) = mean(f_aero2, 3);
     f_a_m(:, :, i, :) = f_a_m_temp;
 
-    M_a_m(:, :, i) = mean(M_aero(:,:,:,i), 3);
+%     M_a_m(:, :, i, 1) = mean(M_aero(:,:,:,i), 3);
+    M_aero1 = M_aero(:, :, :, i); M_aero2 = M_aero(:, :, :, i);
+    M_aero1(M_aero1 < 0) = 0;
+    M_aero2(M_aero2 > 0) = 0;
+    M_a_m_temp = zeros(size(M_a_m(:, :, i, :)));
+    M_a_m_temp(:, :, 1, 1) = mean(M_aero1, 3);
+    M_a_m_temp(:, :, 1, 2) = mean(M_aero2, 3);
+    M_a_m(:, :, i, :) = M_a_m_temp;
 end
 % f_a_m = abs(f_a_m);
 
-params.df_a_1_by_dphi_m = [lin_reg(eps', squeeze(f_a_m(1,1,:,1))), ...
-    lin_reg(eps', squeeze(f_a_m(1,1,:,2)))]; % dphi_m_R > 0, dphi_m_L > 0
-params.df_a_3_by_dphi_m = lin_reg(eps', squeeze(f_a_m(1,3,:,2))); % dphi_m_R > 0, dphi_m_L > 0
-params.df_a_1_by_dtheta_0 = [lin_reg(eps', squeeze(f_a_m(2,1,:,1))), ...
-    lin_reg(eps', squeeze(f_a_m(2,1,:,2)))]; % dtheta_0 > 0
-params.df_a_3_by_dtheta_0 = lin_reg(eps', squeeze(f_a_m(2,3,:,2))); % dtheta_0 > 0
-params.df_a_2_by_dphi_m = [lin_reg(eps(eps'>0)', squeeze(f_a_m(3,2,eps'>0,1))), ...
-    lin_reg(eps(eps'<0)', squeeze(f_a_m(3,2,eps'<0,1)))]; % dphi_m_R > 0, dphi_m_L < 0;
-% But which one to use is undecided - depends on the stroke probably;
-% downstroke : use +ve value, upstroke : use -ve value
-params.df_a_1_by_dtheta_A_m = [lin_reg(eps', squeeze(f_a_m(4,1,:,1))), ...
-    lin_reg(eps', squeeze(f_a_m(4,1,:,2)))]; % dtheta_A_m > 0
-params.df_a_3_by_dtheta_A_m = [lin_reg(eps', squeeze(f_a_m(4,3,:,1))), ...
-    lin_reg(eps', squeeze(f_a_m(4,3,:,2)))]; % dtheta_A_m > 0
-
+params.df_a_1_by_dphi_ms = [lin_reg(eps', squeeze(f_a_m(1,1,:,1))), ...
+    lin_reg(eps', squeeze(f_a_m(1,1,:,2)))];
+params.df_a_3_by_dphi_ms = lin_reg(eps', squeeze(f_a_m(1,3,:,2)));
+params.df_a_1_by_dtheta_0s = [lin_reg(eps', squeeze(f_a_m(2,1,:,1))), ...
+    lin_reg(eps', squeeze(f_a_m(2,1,:,2)))];
+params.df_a_3_by_dtheta_0s = lin_reg(eps', squeeze(f_a_m(2,3,:,2)));
+params.df_a_2_by_dphi_mk = max_abs([lin_reg(eps(eps'>0)', squeeze(f_a_m(3,2,eps'>0,1))), ...
+    lin_reg(eps(eps'<0)', squeeze(f_a_m(3,2,eps'<0,1)))]);
+% But which one to use is undecided - use the one with the larger magnitude;
+params.df_a_1_by_dphi_0s = [lin_reg(eps', squeeze(f_a_m(4,1,:,1))), ...
+    lin_reg(eps', squeeze(f_a_m(4,1,:,2)))];
+params.df_a_3_by_dphi_0s = lin_reg(eps', squeeze(f_a_m(4,3,:,2)));
+params.df_a_2_by_dtheta_0k = max_abs([lin_reg(eps(eps'>0)', squeeze(f_a_m(5,2,eps'>0,1))), ...
+    lin_reg(eps(eps'<0)', squeeze(f_a_m(5,2,eps'<0,1)))]);
+params.df_a_2_by_dpsi_0k = max_abs([lin_reg(eps(eps'>0)', squeeze(f_a_m(6,2,eps'>0,1))), ...
+    lin_reg(eps(eps'<0)', squeeze(f_a_m(6,2,eps'<0,1)))]);
+%
+% params.df_a_1_by_dtheta_A_m = [lin_reg(eps', squeeze(f_a_m(4,1,:,1))), ...
+%     lin_reg(eps', squeeze(f_a_m(4,1,:,2)))];
+% params.df_a_3_by_dtheta_A_m = [lin_reg(eps', squeeze(f_a_m(4,3,:,1))), ...
+%     lin_reg(eps', squeeze(f_a_m(4,3,:,2)))];
+%
+params.dM_a_2_by_dphi_ms = [lin_reg(eps', squeeze(M_a_m(1,2,:,1))), ...
+    lin_reg(eps', squeeze(f_a_m(1,2,:,2)))];
+params.dM_a_2_by_dtheta_0s = [lin_reg(eps', squeeze(M_a_m(2,2,:,1))), ...
+    lin_reg(eps', squeeze(M_a_m(2,2,:,2)))];
+params.dM_a_1_by_dphi_mk = max_abs([lin_reg(eps(eps'>0)', squeeze(M_a_m(3,1,eps'>0,1))), ...
+    lin_reg(eps(eps'<0)', squeeze(M_a_m(3,1,eps'<0,1)))]);
+params.dM_a_3_by_dphi_mk = max_abs([lin_reg(eps(eps'>0)', squeeze(M_a_m(3,3,eps'>0,1))), ...
+    lin_reg(eps(eps'<0)', squeeze(M_a_m(3,3,eps'<0,1)))]);
+params.dM_a_2_by_dphi_0s = [lin_reg(eps', squeeze(M_a_m(4,2,:,1))), ...
+    lin_reg(eps', squeeze(M_a_m(4,2,:,2)))];
+params.dM_a_1_by_dtheta_0k = max_abs([lin_reg(eps(eps'>0)', squeeze(M_a_m(5,1,eps'>0,1))), ...
+    lin_reg(eps(eps'<0)', squeeze(M_a_m(5,1,eps'<0,1)))]);
+params.dM_a_3_by_dtheta_0k = max_abs([lin_reg(eps(eps'>0)', squeeze(M_a_m(5,3,eps'>0,1))), ...
+    lin_reg(eps(eps'<0)', squeeze(M_a_m(5,3,eps'<0,1)))]);
+params.dM_a_1_by_dpsi_0k = max_abs([lin_reg(eps(eps'>0)', squeeze(M_a_m(6,1,eps'>0,1))), ...
+    lin_reg(eps(eps'<0)', squeeze(M_a_m(6,1,eps'<0,1)))]);
+params.dM_a_3_by_dpsi_0k = max_abs([lin_reg(eps(eps'>0)', squeeze(M_a_m(6,3,eps'>0,1))), ...
+    lin_reg(eps(eps'<0)', squeeze(M_a_m(6,3,eps'<0,1)))]);
 %%
 % Get a list of all variables
 allvars = whos;
@@ -69,8 +101,8 @@ end
 
 function [f_aero, M_aero] = param_study(INSECT, WK, eps, i, t, X0, N, R, F_R)
 %%
-    f_aero = zeros(5, 3, N);
-    M_aero = zeros(5, 3, N);
+    f_aero = zeros(7, 3, N);
+    M_aero = zeros(7, 3, N);
     %
     WK_R = WK;  WK_L = WK;
     WK_R.phi_m = WK_R.phi_m + eps(i);
@@ -80,32 +112,34 @@ function [f_aero, M_aero] = param_study(INSECT, WK, eps, i, t, X0, N, R, F_R)
     WK_R = WK;  WK_L = WK;
     WK_R.theta_0 = WK_R.theta_0 + eps(i);
     WK_L.theta_0 = WK_L.theta_0 + eps(i);
-    f_aero(2, :, :) = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R, F_R);
-    %
-    WK_R = WK;  WK_L = WK;
-    WK_R.psi_m = WK_R.psi_m + eps(i);
-    WK_L.psi_m = WK_L.psi_m + eps(i);
-    f_aero(3, :, :) = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R, F_R);
+    [f_aero(2, :, :), M_aero(2, :, :)] = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R, F_R);
     %
     WK_R = WK;  WK_L = WK;
     WK_R.phi_m = WK_R.phi_m + eps(i);
     WK_L.phi_m = WK_L.phi_m - eps(i);
-    [f_aero(4, :, :), M_aero(2, :, :)] = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R, F_R);
+    [f_aero(3, :, :), M_aero(3, :, :)] = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R, F_R);
+    %
+    WK_R = WK;  WK_L = WK;
+    WK_R.phi_0 = WK_R.phi_0 + eps(i);
+    WK_L.phi_0 = WK_L.phi_0 + eps(i);
+    [f_aero(4, :, :), M_aero(4, :, :)] = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R, F_R);
     %
     WK_R = WK;  WK_L = WK;
     WK_R.theta_0 = WK_R.theta_0 + eps(i);
     WK_L.theta_0 = WK_L.theta_0 - eps(i);
-    [~, M_aero(3, :, :)] = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R, F_R);
+    [f_aero(5, :, :), M_aero(5, :, :)] = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R, F_R);
     %
     WK_R = WK;  WK_L = WK;
-    WK_R.psi_m = WK_R.psi_m + eps(i);
-    WK_L.psi_m = WK_L.psi_m - eps(i);
-    [~, M_aero(4, :, :)] = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R, F_R);
+%     WK_R.phi_0 = WK_R.phi_0 + eps(i);
+%     WK_L.phi_0 = WK_L.phi_0 - eps(i);
+    WK_R.psi_0 = WK_R.psi_0 + eps(i);
+    WK_L.psi_0 = WK_L.psi_0 - eps(i);
+    [f_aero(6, :, :), M_aero(6, :, :)] = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R, F_R);
     %
     WK_R = WK;  WK_L = WK;
     WK_R.theta_A_m = WK_R.theta_A_m + eps(i);
     WK_L.theta_A_m = WK_L.theta_A_m + eps(i);
-    [~, M_aero(5, :, :), f_aero(5, :, :)] = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R, F_R);
+    [~, M_aero(7, :, :), f_aero(7, :, :)] = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R, F_R);
 end
 
 function [f_aero, M_aero, f_abd] = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, N, R_id, F_R_id)
@@ -113,11 +147,18 @@ function [f_aero, M_aero, f_abd] = aerodynamic_param(INSECT, WK_R, WK_L, t, X0, 
     [t,X]=ode45(@(t,X) eom_QS_xR(INSECT, WK_R, WK_L, t, X), t, X0, odeset('AbsTol',1e-6,'RelTol',1e-6));
     f_aero = zeros(3, N); f_abd = zeros(3, N); M_aero = zeros(3, N);
     for k=1:N    
-        [~, R, ~, ~, Q_A, ~, W, ~, ~, ~, ~, W_A, W_A_dot, ~, ~, ~, ~, f_a, ~, f_tau]= eom_QS_xR(INSECT, WK_R, WK_L, t(k), X(k,:)');
+        [~, R, Q_R, Q_L, Q_A, ~, W, ~, ~, ~, ~, W_A, W_A_dot, ~, ~, M_R, M_L, f_a, ~, f_tau]= eom_QS_xR(INSECT, WK_R, WK_L, t(k), X(k,:)');
         [JJ_A, KK_A] = inertia_wing_sub(INSECT.m_A, INSECT.mu_A, INSECT.nu_A, INSECT.J_A, R, Q_A, X(k, 4:6)', W, W_A);
         f_abd(:, k) = -(JJ_A(1:3, 7:9)*W_A_dot + KK_A(1:3, 7:9)*W_A);
         f_aero(:, k) = f_a(1:3);
-        M_aero(:, k) = f_a(4:6) + f_tau(4:6);
+%         M_aero(:, k) = f_a(4:6);
+%         M_aero(:, k) = f_a(4:6) + f_tau(4:6);
+        M_aero(:, k) = f_a(4:6) + Q_R*M_R + Q_L*M_L;
     end
     f_total = f_aero + f_abd;
+end
+
+function out = max_abs(inp)
+[~, I] = max(abs(inp));
+out = inp(I);
 end
--- matlab/plotting/plot_parametric_study.m
+++ matlab/plotting/plot_parametric_study.m
@@ -2,75 +2,132 @@
 addpath('../modules', '../sim_data', '../');
 set(0,'DefaultAxesFontName','times');
 set(0,'DefaultAxesFontSize',18);
-load('parametric_study_xR.mat');
+type = 'xR'; % types = {'x', 'xR', 'full'}
 
-h_f_a_long = figure;
-h_f_a_long.PaperUnits = 'inches';
-h_f_a_long.PaperPosition = [0 0 13 9];
-nr = 2;
-nc = 3;
-%
-ic = 1;
+%%
+if strcmp(type, 'x')
+    load('parametric_study.mat');
+    h_f_a_long = figure;
+    h_f_a_long.PaperUnits = 'inches';
+    h_f_a_long.PaperPosition = [0 0 13 9];
+    nr = 2;
+    nc = 3;
+    %
+    ic = 1;
 
-ir = 1;
-subplot(nr,nc,ic);
-plot(eps, squeeze(f_a_m(ic,ir,:,1)));
-ylabel('$\bar f_{a_1}$','interpreter','latex');
-hold on;
-yyaxis right;
-plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
-title(sprintf('$m_p$ = %0.3e \n $m_n$ = %0.3e\n', params.df_a_1_by_dphi_m), 'interpreter','latex');
+    ir = 1;
+    subplot(nr,nc,ic);
+    plot(eps, squeeze(f_a_m(ic,ir,:,1)));
+    ylabel('$\bar f_{a_1}$','interpreter','latex');
+    hold on;
+    yyaxis right;
+    plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
+    title(sprintf('$m_p$ = %0.3e \n $m_n$ = %0.3e\n', params.df_a_1_by_dphi_m), 'interpreter','latex');
 
-ir = 3;
-subplot(nr,nc,ic+nc);
-plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
-xlabel(sprintf('$\\Delta\\phi_{m_s}$'),'interpreter','latex');
-ylabel('$\bar f_{a_3}$','interpreter','latex');
-title(sprintf('$m_n$ = %0.3e\n', params.df_a_3_by_dphi_m),'interpreter','latex');
-%
-ic = 2;
+    ir = 3;
+    subplot(nr,nc,ic+nc);
+    plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
+    xlabel(sprintf('$\\Delta\\phi_{m_s}$'),'interpreter','latex');
+    ylabel('$\bar f_{a_3}$','interpreter','latex');
+    title(sprintf('$m_n$ = %0.3e\n', params.df_a_3_by_dphi_m),'interpreter','latex');
+    %
+    ic = 2;
 
-ir = 1;
-subplot(nr,nc,ic);
-plot(eps, squeeze(f_a_m(ic,ir,:,1)));
-hold on;
-yyaxis right;
-plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
-title(sprintf('$m_p$ = %0.3e \n $m_n$ = %0.3e\n', params.df_a_1_by_dtheta_0),'interpreter','latex');
+    ir = 1;
+    subplot(nr,nc,ic);
+    plot(eps, squeeze(f_a_m(ic,ir,:,1)));
+    hold on;
+    yyaxis right;
+    plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
+    title(sprintf('$m_p$ = %0.3e \n $m_n$ = %0.3e\n', params.df_a_1_by_dtheta_0),'interpreter','latex');
 
-ir = 3;
-subplot(nr,nc,ic+nc);
-plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
-xlabel(sprintf('$\\Delta\\theta_0$'),'interpreter','latex');
-title(sprintf('$m_n$ = %0.3e\n', params.df_a_3_by_dtheta_0),'interpreter','latex');
-%
-ic = 4;
+    ir = 3;
+    subplot(nr,nc,ic+nc);
+    plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
+    xlabel(sprintf('$\\Delta\\theta_0$'),'interpreter','latex');
+    title(sprintf('$m_n$ = %0.3e\n', params.df_a_3_by_dtheta_0),'interpreter','latex');
+    %
+    ic = 4;
 
-ir = 1;
-subplot(nr,nc,ic-1);
-plot(eps, squeeze(f_a_m(ic,ir,:,1)));
-hold on;
-yyaxis right;
-plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
-title(sprintf('$m_p$ = %0.3e \n $m_n$ = %0.3e\n', params.df_a_1_by_dtheta_A_m),'interpreter','latex');
+    ir = 1;
+    subplot(nr,nc,ic-1);
+    plot(eps, squeeze(f_a_m(ic,ir,:,1)));
+    hold on;
+    yyaxis right;
+    plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
+    title(sprintf('$m_p$ = %0.3e \n $m_n$ = %0.3e\n', params.df_a_1_by_dtheta_A_m),'interpreter','latex');
 
-ir = 3;
-subplot(nr,nc,ic+nc-1);
-plot(eps, squeeze(f_a_m(ic,ir,:,1)));
-hold on;
-yyaxis right;
-plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
-xlabel('$\Delta\theta_{A_m}$','interpreter','latex');
-title(sprintf('$m_p$ = %0.3e \n $m_n$ = %0.3e\n', params.df_a_3_by_dtheta_A_m),'interpreter','latex');
-%
-print(h_f_a_long, 'hover_param_study_long', '-depsc', '-r0');
+    ir = 3;
+    subplot(nr,nc,ic+nc-1);
+    plot(eps, squeeze(f_a_m(ic,ir,:,1)));
+    hold on;
+    yyaxis right;
+    plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
+    xlabel('$\Delta\theta_{A_m}$','interpreter','latex');
+    title(sprintf('$m_p$ = %0.3e \n $m_n$ = %0.3e\n', params.df_a_3_by_dtheta_A_m),'interpreter','latex');
+    %
+    print(h_f_a_long, 'hover_param_study_long', '-depsc', '-r0');
 
-set(0,'DefaultAxesFontSize',24);
-h_f_a_lat = figure;
-plot(eps(eps<0), squeeze(f_a_m(3,2,eps<0,1)));
-ylabel('$\bar f_{a_2}$','interpreter','latex');
-hold on;
-plot(eps(eps>0), squeeze(f_a_m(3,2,eps>0,2)), 'r');
-xlabel(sprintf('$\\Delta\\phi_{m_k}$'),'interpreter','latex');
-title(sprintf('$m$ = %0.3e\n', params.df_a_2_by_dphi_m(2)),'interpreter','latex');
-print(h_f_a_lat, 'hover_param_study_lat', '-depsc', '-r0');
+    set(0,'DefaultAxesFontSize',24);
+    h_f_a_lat = figure;
+    plot(eps(eps<0), squeeze(f_a_m(3,2,eps<0,1)));
+    ylabel('$\bar f_{a_2}$','interpreter','latex');
+    hold on;
+    plot(eps(eps>0), squeeze(f_a_m(3,2,eps>0,2)), 'r');
+    xlabel(sprintf('$\\Delta\\phi_{m_k}$'),'interpreter','latex');
+    title(sprintf('$m$ = %0.3e\n', params.df_a_2_by_dphi_m(2)),'interpreter','latex');
+    print(h_f_a_lat, 'hover_param_study_lat', '-depsc', '-r0');
+end
+
+%%
+if strcmp(type, 'xR')
+    load('parametric_study_xR.mat');
+    xlabels = {'$\Delta\phi_{m_s}$', '$\Delta\theta_{0_s}$', '$\Delta\phi_{m_k}$',...
+        '$\Delta\phi_{0_s}$', '$\Delta\theta_{0_k}$', '$\Delta\psi_{0_k}$'};
+    ylabels_f = {'$\bar f_{a_1}$', '$\bar f_{a_2}$' '$\bar f_{a_3}$'};
+    ylabels_M = {'$\bar M_{a_1}$', '$\bar M_{a_2}$' '$\bar M_{a_3}$'};
+    nr = 3;
+    nc = 6;
+    %
+    h_f_a = figure;
+    h_f_a.PaperUnits = 'inches';
+    h_f_a.PaperPosition = [0 0 16 7];
+    for ir=1:3
+        for ic=1:6
+            subplot(nr,nc,(ir-1)*nc+ic);
+            plot(eps, squeeze(f_a_m(ic,ir,:,1)));
+            if ic==1
+                ylabel(ylabels_f{ir},'interpreter','latex');
+            end
+            hold on;
+            yyaxis right;
+            plot(eps, squeeze(f_a_m(ic,ir,:,2)), 'r');
+            if ir==3
+                xlabel(xlabels{ic},'interpreter','latex');
+            end
+        end
+    end
+    print(h_f_a, 'hover_param_study_xRf', '-depsc', '-r0');
+    %
+    h_f_a = figure;
+    h_f_a.PaperUnits = 'inches';
+    h_f_a.PaperPosition = [0 0 16 7];
+    for ir=1:3
+        for ic=1:6
+            subplot(nr,nc,(ir-1)*nc+ic);
+            plot(eps, squeeze(M_a_m(ic,ir,:,1)));
+            if ic==1
+                ylabel(ylabels_M{ir},'interpreter','latex');
+            end
+            hold on;
+            yyaxis right;
+            plot(eps, squeeze(M_a_m(ic,ir,:,2)), 'r');
+            if ir==3
+                xlabel(xlabels{ic},'interpreter','latex');
+            end
+        end
+    end
+    print(h_f_a, 'hover_param_study_xRM', '-depsc', '-r0');
+
+%     title(sprintf('$m_p$ = %0.3e \n $m_n$ = %0.3e\n', params.df_a_1_by_dphi_m), 'interpreter','latex');
+end
