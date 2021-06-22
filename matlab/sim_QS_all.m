d72345af13280226a847b35cb1e52dd7db570ff3
Tejaswi K C <kctejaswi999@gmail.com>
2021-06-22 10:28:34 -0400

    4th order Crouch Grossman

--- matlab/sim_QS_all.m
+++ matlab/sim_QS_all.m
@@ -25,12 +25,12 @@ x0=rand(3,1);
 X0=[x0; reshape(R0,9,1); reshape(Q_R0,9,1); reshape(Q_L0,9,1); reshape(Q_A0,9,1);...
     x_dot0; W0; W_R0; W_L0; W_A0];
 
-N_period = 50;
-N_single = 100;
+N_period = 1;
+N_single = 50;
 N_iters = 10;
-integrator = 'crgr3'; % ode45, crgr2, crgr3
-
 N_per_iter = N_single / N_iters;
+integrator = 'crgr4'; % ode45, ode45ct, crgr2, crgr3, crgr4
+
 N = 1 + N_period * N_single;
 t=linspace(0,1/f*N_period,N);
 X = zeros(N, length(X0));
@@ -48,6 +48,12 @@ switch integrator
     end
 %     [~, X] = ode45(@(t,X) eom(INSECT,t,X), t, X0, odeset('AbsTol',1e-6,'RelTol',1e-6));
     time_ode45 = toc;
+    
+    case 'ode45ct'
+    % Continuous ode45
+    tic;
+    [~, X] = ode45(@(t,X) eom(INSECT,t,X), t, X0, odeset('AbsTol',1e-6,'RelTol',1e-6));
+    time_ode45ct = toc;
 
     case 'crgr2'
     % 2nd order Crouch Grossman
@@ -72,6 +78,21 @@ switch integrator
         X(k+1, :) = crouch_grossman_3rd(INSECT, X(k, :)', dt, a, b);
     end
     time_crgr3 = toc;
+    
+    case 'crgr4'
+    % 4th order Crouch grossman
+    a = [0, 0, 0, 0, 0;
+        0.8177227988124852, 0, 0, 0, 0;
+        0.3199876375476427, 0.0659864263556022, 0, 0, 0;
+        0.9214417194464946, 0.4997857776773573, -1.0969984448371582, 0, 0;
+        0.3552358559023322, 0.2390958372307326, 1.3918565724203246, -1.1092979392113465, 0];
+    b = [0.1370831520630755, -0.0183698531564020, 0.7397813985370780, -0.1907142565505889, 0.3322195591068374];
+    c = [0, 0.8177227988124852, 0.3859740639032449, 0.3242290522866937, 0.8768903263420429];
+    tic;
+    for k=1:N-1
+        X(k+1, :) = crouch_grossman_4th(INSECT, X(k, :)', dt, a, b);
+    end
+    time_crgr4 = toc;
 end
 
 x=X(:,1:3)';
@@ -214,6 +235,66 @@ Q_Rout = Q_R1 * expmhat(dt*b(1)*W_R1) * expmhat(dt*b(2)*W_R2) * expmhat(dt*b(3)*
 Q_Lout = Q_L1 * expmhat(dt*b(1)*W_L1) * expmhat(dt*b(2)*W_L2) * expmhat(dt*b(3)*W_L3);
 Q_Aout = Q_A1 * expmhat(dt*b(1)*W_A1) * expmhat(dt*b(2)*W_A2) * expmhat(dt*b(3)*W_A3);
 xiout = xi1 + dt* (b(1)* K1_xi + b(2)* K2_xi + b(3)* K3_xi);
+
+Xout = [xout; reshape(Rout, 9, 1); reshape(Q_Rout, 9, 1); reshape(Q_Lout, 9, 1); reshape(Q_Aout, 9, 1); xiout];
+end
+
+function Xout = crouch_grossman_4th(INSECT, X, dt, a, b)
+%%
+x1=X(1:3);
+R1=reshape(X(4:12),3,3);
+Q_R1=reshape(X(13:21),3,3);
+Q_L1=reshape(X(22:30),3,3);
+Q_A1=reshape(X(31:39),3,3);
+x_dot1=X(40:42);
+W1=X(43:45);
+W_R1=X(46:48);
+W_L1=X(49:51);
+W_A1=X(52:54);
+
+xi1=X(40:54);
+K1_xi = deriv_xi(INSECT, x1, R1, Q_R1, Q_L1, Q_A1, x_dot1, W1, W_R1, W_L1, W_A1);
+
+x2 = x1 + dt*a(2,1)* x_dot1;
+R2 = R1 * expmhat(dt*a(2,1)*W1); Q_R2 = Q_R1 * expmhat(dt*a(2,1)*W_R1);
+Q_L2 = Q_L1 * expmhat(dt*a(2,1)*W_L1); Q_A2 = Q_A1 * expmhat(dt*a(2,1)*W_A1);
+xi2 = xi1 + dt*a(2,1)* K1_xi;
+x_dot2 = xi2(1:3); W2 = xi2(4:6); W_R2 = xi2(7:9); W_L2 = xi2(10:12); W_A2 = xi2(13:15);
+K2_xi = deriv_xi(INSECT, x2, R2, Q_R2, Q_L2, Q_A2, x_dot2, W2, W_R2, W_L2, W_A2);
+
+x3 = x1 + dt* (a(3,1)* x_dot1 + a(3,2)* x_dot2);
+R3 = R1 * expmhat(dt*a(3,1)*W1) * expmhat(dt*a(3,2)*W2);
+Q_R3 = Q_R1 * expmhat(dt*a(3,1)*W_R1) * expmhat(dt*a(3,2)*W_R2);
+Q_L3 = Q_L1 * expmhat(dt*a(3,1)*W_L1) * expmhat(dt*a(3,2)*W_L2);
+Q_A3 = Q_A1 * expmhat(dt*a(3,1)*W_A1) * expmhat(dt*a(3,2)*W_A2);
+xi3 = xi1 + dt* (a(3,1)* K1_xi + a(3,2)* K2_xi);
+x_dot3 = xi3(1:3); W3 = xi3(4:6); W_R3 = xi3(7:9); W_L3 = xi3(10:12); W_A3 = xi3(13:15);
+K3_xi = deriv_xi(INSECT, x3, R3, Q_R3, Q_L3, Q_A3, x_dot3, W3, W_R3, W_L3, W_A3);
+
+x4 = x1 + dt* (a(4,1)* x_dot1 + a(4,2)* x_dot2 + a(4,3)* x_dot3);
+R4 = R1 * expmhat(dt*a(4,1)*W1) * expmhat(dt*a(4,2)*W2) * expmhat(dt*a(4,3)*W3);
+Q_R4 = Q_R1 * expmhat(dt*a(4,1)*W_R1) * expmhat(dt*a(4,2)*W_R2) * expmhat(dt*a(4,3)*W_R3);
+Q_L4 = Q_L1 * expmhat(dt*a(4,1)*W_L1) * expmhat(dt*a(4,2)*W_L2) * expmhat(dt*a(4,3)*W_L3);
+Q_A4 = Q_A1 * expmhat(dt*a(4,1)*W_A1) * expmhat(dt*a(4,2)*W_A2) * expmhat(dt*a(4,3)*W_A3);
+xi4 = xi1 + dt* (a(4,1)* K1_xi + a(4,2)* K2_xi + a(4,3)* K3_xi);
+x_dot4 = xi4(1:3); W4 = xi4(4:6); W_R4 = xi4(7:9); W_L4 = xi4(10:12); W_A4 = xi4(13:15);
+K4_xi = deriv_xi(INSECT, x4, R4, Q_R4, Q_L4, Q_A4, x_dot4, W4, W_R4, W_L4, W_A4);
+
+x5 = x1 + dt* (a(5,1)* x_dot1 + a(5,2)* x_dot2 + a(5,3)* x_dot3 + a(5,4)* x_dot4);
+R5 = R1 * expmhat(dt*a(5,1)*W1) * expmhat(dt*a(5,2)*W2) * expmhat(dt*a(5,3)*W3) * expmhat(dt*a(5,4)*W4);
+Q_R5 = Q_R1 * expmhat(dt*a(5,1)*W_R1) * expmhat(dt*a(5,2)*W_R2) * expmhat(dt*a(5,3)*W_R3) * expmhat(dt*a(5,4)*W_R4);
+Q_L5 = Q_L1 * expmhat(dt*a(5,1)*W_L1) * expmhat(dt*a(5,2)*W_L2) * expmhat(dt*a(5,3)*W_L3) * expmhat(dt*a(5,4)*W_L4);
+Q_A5 = Q_A1 * expmhat(dt*a(5,1)*W_A1) * expmhat(dt*a(5,2)*W_A2) * expmhat(dt*a(5,3)*W_A3) * expmhat(dt*a(5,4)*W_A4);
+xi5 = xi1 + dt* (a(5,1)* K1_xi + a(5,2)* K2_xi + a(5,3)* K3_xi + a(5,4)* K4_xi);
+x_dot5 = xi5(1:3); W5 = xi5(4:6); W_R5 = xi5(7:9); W_L5 = xi5(10:12); W_A5 = xi5(13:15);
+K5_xi = deriv_xi(INSECT, x5, R5, Q_R5, Q_L5, Q_A5, x_dot5, W5, W_R5, W_L5, W_A5);
+
+xout = x1 + dt* (b(1)* x_dot1 + b(2)* x_dot2 + b(3)* x_dot3 + b(4)* x_dot4 + b(5)* x_dot5);
+Rout = R1 * expmhat(dt*b(1)*W1) * expmhat(dt*b(2)*W2) * expmhat(dt*b(3)*W3) * expmhat(dt*b(4)*W4) * expmhat(dt*b(5)*W5);
+Q_Rout = Q_R1 * expmhat(dt*b(1)*W_R1) * expmhat(dt*b(2)*W_R2) * expmhat(dt*b(3)*W_R3) * expmhat(dt*b(4)*W_R4) * expmhat(dt*b(5)*W_R5);
+Q_Lout = Q_L1 * expmhat(dt*b(1)*W_L1) * expmhat(dt*b(2)*W_L2) * expmhat(dt*b(3)*W_L3) * expmhat(dt*b(4)*W_L4) * expmhat(dt*b(5)*W_L5);
+Q_Aout = Q_A1 * expmhat(dt*b(1)*W_A1) * expmhat(dt*b(2)*W_A2) * expmhat(dt*b(3)*W_A3) * expmhat(dt*b(4)*W_A4) * expmhat(dt*b(5)*W_A5);
+xiout = xi1 + dt* (b(1)* K1_xi + b(2)* K2_xi + b(3)* K3_xi + b(4)* K4_xi + b(5)* K5_xi);
 
 Xout = [xout; reshape(Rout, 9, 1); reshape(Q_Rout, 9, 1); reshape(Q_Lout, 9, 1); reshape(Q_Aout, 9, 1); xiout];
 end
